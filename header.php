496d476312a14d512a7d0b436ff1f05887fea047
varrel <nugrahavarel@gmail.com>
2023-06-08 17:53:11 +0700

    update header

--- header.php
+++ header.php
@@ -4,38 +4,37 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/styleheader.css">
     <title>Document</title>
 </head>
 <body>
     <nav>
-    <div class="wrapper">
-            <div class="menu" style="float:left">
-                <ul>
-                    <li><a href="index.php">Home</a></li>
-                    <li><a href="">Category</a>
-                        <ul class="dropdown">
-                            <li><a href="<?php echo "kategori.php?kategori=tenda" ?>">Tenda</a></li>
-                            <li><a href="<?php echo "kategori.php?kategori=memasak" ?>">Memasak</a></li>
-                            <li><a href="<?php echo "kategori.php?kategori=pencahayaan" ?>">Pencahayan</a></li>
-                            <li><a href="<?php echo "kategori.php?kategori=makanan" ?>">Makanan</a></li>
-                        </ul>
-                    </li>
-                    <li><a href="#footer">Contact</a></li>
-                    <li><a href="pesanan.php">Penyewaan</a></li>
-                </ul>
+        <div class="wrapper">
+            <div class="logo">
+                <img src="resource/logo.png" alt="Logo" />
             </div>
-                <div style="float:left; width:32%;">   
-                    <img src = "resource/logo.png" style="width:180px; height :49px;float:right; margin-top:1.5%;margin-left:0%;margin-bottom:1.5%"> 
-                </div>
-            
-                <div class="logout" style="float: right; padding: 0px 20px 0px 20px;">         
+            <ul class="menu">
+                <li class="nav-item"><a href="index.php">Home</a></li>
+                <li class="nav-item">
+                    <a href="#">Category</a>
+                    <ul class="dropdown">
+                        <li><a href="kategori.php?kategori=tenda">Tenda</a></li>
+                        <li><a href="kategori.php?kategori=memasak">Memasak</a></li>
+                        <li><a href="kategori.php?kategori=pencahayaan">Pencahayan</a></li>
+                        <li><a href="kategori.php?kategori=makanan">Makanan</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item"><a href="#footer">Contact</a></li>
+                <li class="nav-item"><a href="pesanan.php">Penyewaan</a></li>
+            </ul>
+            <div class="logout">
                 <?php 
                 if(isset($_SESSION['id'])){
                     echo $_SESSION['email'];
                 }
                 ?>
-                    <a href="logout.php"><button class="buttonlogout button5">Logout</button></a>
-                </div>     
+                <a href="logout.php" class="buttonlogout button5">Logout</a>
+            </div>
         </div>
     </nav>
 </body>
