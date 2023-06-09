5c100820d29b0624c1ea9f7fac1c44c6d020c4e4
varrel <nugrahavarel@gmail.com>
2023-06-09 18:05:51 +0700

    accept curent change dinda

--- index.php
+++ index.php
@@ -13,11 +13,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="resource/icon.png">
     <link rel="stylesheet" href="style/styleindex.css">
-<<<<<<< HEAD
     <title>Lembah Indah CampGround</title>
-=======
-    <title>Lembah Indah Campground</title>
->>>>>>> a5aceed2bc779cf9058b22a5d0053aa307792341
 </head>
 <body>
     <?php 
