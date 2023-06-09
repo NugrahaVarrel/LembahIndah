0c6d21fe8d149e3e559fe88f24ee174c854e7660
Adinda Putri <adindaputrii333@gmail.com>
2023-06-09 16:50:25 +0700

    rev

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style/styleheader.css">
-    <title>Document</title>
+    <title>Lembah Indah Campground</title>
 </head>
 <body>
     <nav>
