38c13b93cd7eb99da7bc608b01663891affb41fc
varrel <nugrahavarel@gmail.com>
2023-06-09 10:04:09 +0700

    update

--- kategori.php
+++ kategori.php
@@ -28,29 +28,29 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" href="resource/icon.png">
+    <link rel="stylesheet" href="style/stylekategori.css">
     <title>Document</title>
 </head>
 <body>
     <?php include('header.php');?>
     <div id="cdm" class="boxcdr">
         <section class="cdr">
-            <br>Kategori Barang
+            <img src="resource/icon_tenda.png" alt="" class="icon">
+            <span class="kategori">Kategori Barang</span>
         </section>
-    </div><br>
-    <?php while($data = mysqli_fetch_array($result)){
-            ?>
-            <div class="flexbox1">
-                <img src="admin/produk/<?php echo $data['foto']; ?>">
-                <br><br>
-                <div class="lain3">
-                    <?php echo $data['nama']."<br><br>"; ?>
-                </div> 
-                <div class="lain3">
-                    <?php echo $data['harga']; ?>/minggu
-                </div>
-                <br>
-                <a href="<?php echo "detail_barang.php?id_produk=".$data['id_produk']; ?>"><button class="tbl-biru">Sewa</button></a>                 
+    </div>
+    <br>
+    <?php while($data = mysqli_fetch_array($result)){?>
+        <div class="flexbox1">
+            <div class="nama"><?php echo $data['nama']; ?></div>
+            <img src="admin/produk/<?php echo $data['foto']; ?>">
+            <br><br>
+            <div class="lain3">
+                <?php echo $data['harga']; ?>/minggu
             </div>
+            <br>
+            <a href="<?php echo "detail_barang.php?id_produk=".$data['id_produk']; ?>"><button class="tbl-biru">Sewa</button></a>                 
+        </div>
     <?php } ?>
 <br><br><br><br><br><br><br><br><br>
 <section id="footer">
