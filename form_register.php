87ffdc5a55ad6c6d42dcb8efb7f068810d313cdd
varrel <nugrahavarel@gmail.com>
2023-06-12 18:33:57 +0700

    form register

--- form_register.php
+++ form_register.php
@@ -9,6 +9,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" href="resource/icon-lembahindah.png">
+    <link rel="stylesheet" href="style/styleformregister.css">
     <title>Daftar | Lembah Indah</title>
 </head>
 <body onload=display_ct();>
